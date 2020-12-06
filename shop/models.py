4a93e47f8fb4196d5d889f00d5e2cdd83f4f42c1
Hritik Arora <hritikarora875@gmail.com>
2020-12-06 17:58:24 +0530

    third commit

--- shop/models.py
+++ shop/models.py
@@ -5,4 +5,10 @@ class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
     product_description = models.CharField(max_length=300)
-    publish_date = models.DateField
+    product_catergory = models.CharField(max_length=50, default="")
+    product_subcatergory = models.CharField(max_length=50, default="")
+    product_price= models.IntegerField(default=0)
+    product_image= models.ImageField(upload_to="shop/images", default="")
+    publish_date = models.DateField()
+    def __str__(self):
+        return self.product_name
