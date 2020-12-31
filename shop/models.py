cd53334fa1ea35f0e8cd5268661d784ae29f1ced
Hritik Arora <hritikarora875@gmail.com>
2020-12-31 13:07:52 +0530

    new-commmit

--- shop/models.py
+++ shop/models.py
@@ -10,5 +10,16 @@ class Product(models.Model):
     product_price = models.IntegerField(default=0)
     product_image = models.ImageField(upload_to="shop/images", default="")
     publish_date = models.DateField()
+
     def __str__(self):
         return self.product_name
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, default="")
+    phone = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=500, default="")
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 # Create your views here.
@@ -26,6 +26,16 @@ def About(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    if request.method == "POST":
+        print(request)
+        name = request.POST.get('name','')
+
+        email =request.POST.get('email')
+        phone =request.POST.get('phone')
+        desc =request.POST.get('desc')
+        contact = Contact(name =name, email=email, phone=phone, desc=desc)
+        contact.save()
+
     return render(request, 'shop/contact.html')
 
 def Tracker(request):
